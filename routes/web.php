5816562fd4916e0fb69218f39780bb458b8c6356
maarrdamarr <damardarma42@gmail.com>
2025-11-28 16:17:57 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,25 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+use App\Http\Controllers\BarangController;
+use App\Http\Controllers\LelangController;
+use App\Http\Controllers\PenawaranController;
+use App\Http\Controllers\BeritaController;
+use App\Http\Controllers\DashboardController;
+
+Route::get('/', function () { return view('landing'); })->name('landing');
+
+Route::resource('barang', BarangController::class);
+Route::resource('berita', BeritaController::class);
+
+Route::middleware(['auth'])->group(function(){
+    Route::get('/dashboard', [DashboardController::class,'index'])->name('dashboard');
+
+    Route::middleware('can:isPenjual')->group(function(){
+        Route::resource('lelang', LelangController::class);
+    });
+
+    Route::post('lelang/{lelang}/tawar', [PenawaranController::class,'store'])->name('lelang.tawar');
+});
 
 require __DIR__.'/auth.php';
